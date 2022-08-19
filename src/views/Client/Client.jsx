af465745bcf9fcc48bee13935303586c476405f5
edarcode <edarcode@gmail.com>
2022-08-18 23:39:24 -0500

    feat(src/styles/colors.css) estableciendo fuentes y algunas medidas

--- src/views/Client/Client.jsx
+++ src/views/Client/Client.jsx
@@ -1,9 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../../components/client/Footer/Footer.jsx";
+import Header from "../../components/client/Header/Header.jsx";
+import css from "./style.module.css";
 
 export default function Client() {
 	return (
-		<div>
-			<Outlet />
+		<div className={css.client}>
+			<Header />
+			<div className={css.outlet}>
+				<Outlet />
+			</div>
+			<Footer />
 		</div>
 	);
 }
