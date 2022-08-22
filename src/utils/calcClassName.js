41cb77bdf5ef064e4d5f959158936a6362739a1d
edarcode <edarcode@gmail.com>
2022-08-22 10:32:00 -0500

    fix(src/utils/calcClassName.js) baseClassName ahora acepta una clase compleja

--- src/utils/calcClassName.js
+++ src/utils/calcClassName.js
@@ -1,6 +1,14 @@
 export const calcClassName = (css, { baseClassName, className }) => {
 	let newClassName = "";
-	if (baseClassName) newClassName = css[baseClassName];
+	if (baseClassName) {
+		const splitBaseClassName = baseClassName.split(" ");
+		for (let i = 0; i < splitBaseClassName.length; i++) {
+			const element = splitBaseClassName[i];
+			newClassName
+				? (newClassName += (css[element] && ` ${css[element]}`) || "")
+				: (newClassName += (css[element] && css[element]) || "");
+		}
+	}
 	if (className)
 		newClassName
 			? (newClassName += ` ${className}`)
