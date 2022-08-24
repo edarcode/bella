e2a16ff9d5aaf633c881404b14903f8663016354
edarcode <edarcode@gmail.com>
2022-08-24 17:49:51 -0500

    feat(*) fetch para traer todos los productos al panel de control

--- src/env/endPointsApi.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL_API = import.meta.env.VITE_BASE_URL_API;
-export const DATA_PRODUCTS_CLIENT = `${BASE_URL_API}/products/client`;
--- src/utils/fetch-control-panel/fetchDataProducts.js
+++ src/utils/fetch-control-panel/fetchDataProducts.js
@@ -1,10 +1,10 @@
-import { DATA_PRODUCTS_CLIENT } from "../../constants/endPointsApi.js";
+import { DATA_PRODUCTS } from "../../constants/endPointsApi.js";
 import { axios } from "../axios.js";
 
 export const fetchDataProducts = async () => {
 	const res = await axios({
 		method: "GET",
-		url: DATA_PRODUCTS_CLIENT
+		url: DATA_PRODUCTS
 	});
 
 	return res;
