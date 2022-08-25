3bb8dac25e4a2d52842b0d63b18e8988995d6ac0
edarcode <edarcode@gmail.com>
2022-08-25 00:31:49 -0500

    feat(src/utils/fetch-control-panel/fetchDataProducts.js) fetch para pedir los productos ahora permite querys

--- src/utils/fetch-control-panel/fetchDataProducts.js
+++ src/utils/fetch-control-panel/fetchDataProducts.js
@@ -1,12 +1,40 @@
 import { DATA_PRODUCTS } from "../../constants/endPointsApi.js";
 import { axios } from "../axios.js";
 
-export const fetchDataProducts = async signal => {
+export const fetchDataProducts = async (signal, querys) => {
+	const url = addQueryToUrl(DATA_PRODUCTS, querys);
 	const res = await axios({
 		method: "GET",
-		url: DATA_PRODUCTS,
+		url,
 		signal
 	});
 
 	return res;
+};
+
+const addQueryToUrl = (url, querys) => {
+	if (!querys) return url;
+	const {
+		page,
+		name,
+		active,
+		order,
+		productsPerPage,
+		minSalePrice,
+		maxSalePrice,
+		categoryId
+	} = querys;
+	const newUrl = new URL(url);
+
+	if (page) newUrl.searchParams.append("page", page);
+	if (name) newUrl.searchParams.append("name", name);
+	if (active) newUrl.searchParams.append("active", active);
+	if (order) newUrl.searchParams.append("order", order);
+	if (productsPerPage)
+		newUrl.searchParams.append("productsPerPage", productsPerPage);
+	if (minSalePrice) newUrl.searchParams.append("minSalePrice", minSalePrice);
+	if (maxSalePrice) newUrl.searchParams.append("maxSalePrice", maxSalePrice);
+	if (categoryId) newUrl.searchParams.append("categoryId", categoryId);
+
+	return newUrl.href;
 };
